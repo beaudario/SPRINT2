f51135e83d4c54664062abc94a46d3bf42e2368b
beaudario <30291512+beaudario@users.noreply.github.com>
2019-02-27 11:26:04 +0100

    FINISHED 4.12

---
+++ 4.12/4.12/Program.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4._12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Geef DD-MM-YYYY (bijv. 05-08-2017): ");
+            string Date = Console.ReadLine();
+
+            string yearNumber = Date.Substring(6, 4);
+            string monthNumber = Date.Substring(3, 2);
+
+            int YearNumber = Convert.ToInt16(yearNumber);
+            int MonthNumber = Convert.ToInt16(monthNumber);
+
+            DateTime dateValue = new DateTime(YearNumber, MonthNumber, 01);
+            Console.WriteLine(dateValue.ToString("dddd"));
+            Console.ReadKey();
+        }
+    }
+}
