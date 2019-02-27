e9263bf31e3a588d1d4b943802f52e70a57780f4
beaudario <30291512+beaudario@users.noreply.github.com>
2019-02-27 12:07:15 +0100

    FINISHED 5.2

--- 5.2/5.2/Program.cs
+++ 5.2/5.2/Program.cs
@@ -10,12 +10,12 @@ namespace _5._2
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
+            for (int counter = 1; counter <= 25; counter++)
+            {
+                int square = counter * counter;
+                Console.WriteLine("{0}   {1}", counter, square);
+            }
             Console.ReadKey();
-
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
         }
     }
 }
