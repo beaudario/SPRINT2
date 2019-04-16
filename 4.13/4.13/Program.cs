5cf1f9785c748d3c2195f79f403eccee4d232690
beaudario <30291512+beaudario@users.noreply.github.com>
2019-04-16 22:11:30 +0200

    4.13 Comments

--- 4.13/4.13/Program.cs
+++ 4.13/4.13/Program.cs
@@ -9,15 +9,18 @@ namespace _4._13
     class Program
     {
         static void Main(string[] args)
-        {
+        {   
+            //Variables
             var LastNumber = 0;
 
+            //UserInput
             Console.Write("Geef een getal: ");
             int Number = Convert.ToInt16(Console.ReadLine());
 
+            //SquaringNumber
             var Square = Math.Pow(Number, 2);
 
-
+            //GettingModulo
             if (Number > 100)
             {
                 LastNumber = Convert.ToInt32(Square) % 1000;
@@ -31,7 +34,7 @@ namespace _4._13
                 LastNumber = Convert.ToInt32(Square) % 10;
             }
 
-
+            //ShowingOutcome
             if (LastNumber == Number)
             {
                 Console.WriteLine(Number + " is een automorf getal.");
